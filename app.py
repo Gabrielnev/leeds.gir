b8e62c90c045efc927d6c021af997fcf4046add1
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2024-08-30 15:05:35 -0500

    pesquisa por cnpj

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request
 import requests
-import os
-from dotenv import load_dotenv
-
-# Carregar as variáveis de ambiente do arquivo .env
-load_dotenv()
 
 app = Flask(__name__)
 
-api_base_url = os.getenv("API_BASE_URL")
-api_key = os.getenv("API_KEY")
-port = int(os.getenv("PORT", 5000))
+# Função para consultar a API ReceitaWS
+def consulta_empresa(cnpj):
+    url = f"https://www.receitaws.com.br/v1/cnpj/{cnpj}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
-@app.route('/empresa/<cnpj>', methods=['GET'])
-def get_empresa(cnpj):
-    try:
-        # Faz a requisição à API externa
-        headers = {'Authorization': f'Bearer {api_key}'}
-        response = requests.get(f'{api_base_url}{cnpj}', headers=headers)
-        
-        # Verifica se a requisição foi bem-sucedida
-        if response.status_code == 200:
-            return jsonify(response.json())
+# Rota principal
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota de busca
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    cnpj = request.form.get('cnpj')
+    if cnpj:
+        dados_empresa = consulta_empresa(cnpj)
+        if dados_empresa:
+            return render_template('resultados.html', empresa=dados_empresa)
         else:
-            return jsonify({'error': 'Erro ao buscar dados da empresa'}), response.status_code
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+            return "Erro ao buscar a empresa. Verifique o CNPJ e tente novamente."
+    return "CNPJ não fornecido."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
